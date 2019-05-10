b38ce97708516bbf7c633231d30e37f95f3117ab
waterfaity <995637517@qq.com>
2019-05-10 10:36:19 +0800

    更新 最新

---
+++ waterfairylib/src/main/java/com/waterfairy/widget/AlbumCoverView.java
@@ -0,0 +1,124 @@
+package com.waterfairy.widget;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.util.AttributeSet;
+
+import com.waterfairy.widget.utils.CanvasUtils;
+
+/**
+ * @author water_fairy
+ * @email 995637517@qq.com
+ * @date 2019/4/25 09:55
+ * @info:
+ */
+public class AlbumCoverView extends android.support.v7.widget.AppCompatImageView {
+    private static final String TAG = "album";
+    Runnable runnable;
+    Handler handler;//计时
+    private boolean isPlaying;//播放中
+    boolean hasHandleBackground;//设置背景
+    private float centerX, centerY;//旋转中心
+
+    public AlbumCoverView(Context context) {
+        this(context, null);
+    }
+
+
+    public AlbumCoverView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+    public void pause() {
+        isPlaying = false;
+        if (handler != null) {
+            handler.removeMessages(0);
+        }
+    }
+
+    public void start() {
+        isPlaying = true;
+        if (handler == null) {
+            handler = new Handler();
+            runnable = new Runnable() {
+                @Override
+                public void run() {
+                    if (getDrawable() != null) {
+                        if (centerX == 0 || centerY == 0) {
+
+                            //计算旋转中心
+                            float height = getDrawable().getIntrinsicHeight();
+                            float width = getDrawable().getIntrinsicWidth();
+                            RectF rect = new RectF(0, 0, width, height);
+                            getImageMatrix().mapRect(rect);
+                            centerX = (rect.right + rect.left) / 2;
+                            centerY = (rect.top + rect.bottom) / 2;
+                        }
+                        //旋转角度  0.5F
+                        getImageMatrix().postRotate(0.5F, centerX, centerY);
+                        postInvalidate();
+
+                    }
+                    if (isPlaying) {
+                        handler.postDelayed(runnable, 50);
+                    }
+                }
+            };
+
+        }
+        handler.removeCallbacks(runnable);
+        handler.post(runnable);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        if (w != 0 && h != 0) {
+            initBackGround();
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+    }
+
+    private void initBackGround() {
+        if (hasHandleBackground) return;
+        hasHandleBackground = true;
+        int width = Math.min(getWidth(), getHeight());//绘制最大宽
+        int radiusBig = width / 2;//绘制最大半径
+        int padding = radiusBig / 6;//阴影+留白
+        int shadowWidth = padding / 3;//阴影
+        int circleRadius = radiusBig - shadowWidth;//白色圆
+        //padding
+        setPadding(padding, padding, padding, padding);
+
+        //创建backGroundBitmap
+        Bitmap backgroundBitmap = Bitmap.createBitmap(width, width, Bitmap.Config.ARGB_4444);
+        Canvas canvas = new Canvas(backgroundBitmap);
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+
+        //绘制阴影
+        CanvasUtils.drawShadow(canvas, radiusBig, shadowWidth, paint);
+
+        //绘制圆
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+        paint.setShader(null);
+        paint.setColor(Color.WHITE);
+        canvas.drawCircle(radiusBig, radiusBig, circleRadius, paint);
+
+        //设置backGround
+        Drawable drawable = new BitmapDrawable(getResources(), backgroundBitmap);
+        setBackgroundDrawable(drawable);
+    }
+}
---
+++ waterfairylib/src/main/java/com/waterfairy/widget/CircleProgress.java
@@ -0,0 +1,129 @@
+package com.waterfairy.widget;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.SweepGradient;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+
+import com.waterfairy.widget.baseView.BaseSelfView;
+import com.waterfairy.widget.baseView.OnFloatChangeListener;
+
+/**
+ * @author water_fairy
+ * @email 995637517@qq.com
+ * @date 2019/5/7 09:42
+ * @info: 进度条
+ */
+public class CircleProgress extends BaseSelfView {
+    private float score;
+    private Paint paint;
+    private RectF rectF;
+    private int maxAngle;
+    private float centerX;
+    private float centerY;
+    private int[] colors;  //过度颜色
+    private float[] points;  //过度位置
+    private SweepGradient sweepGradient;//过度
+    private float lineRadius;//线半径;
+    private float realRadius;//半径
+
+    public CircleProgress(Context context) {
+        this(context, null);
+    }
+
+    public CircleProgress(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        paint = new Paint();
+        paint.setStrokeCap(Paint.Cap.ROUND);
+
+        paint.setAntiAlias(true);
+        paint.setColor(Color.RED);
+
+        colors = new int[]{Color.parseColor("#f76f60"), Color.parseColor("#f47264"), Color.parseColor("#f8988a")};
+        points = new float[]{0, 0.3F, 1F};
+
+    }
+
+
+    int angle = 0;
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.rotate(-90, centerX, centerY);
+        //背景
+        paint.setStrokeWidth(2 * lineRadius);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(Color.parseColor("#fee7e5"));
+        paint.setShader(null);
+        canvas.drawArc(rectF, angle, 360 - angle, false, paint);
+        //前景
+        paint.setShader(sweepGradient);
+        canvas.drawArc(rectF, 0, angle, false, paint);
+        //画圆点
+        paint.setShader(null);
+
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+
+        paint.setColor(Color.parseColor("#f76f60"));
+        paint.setStrokeWidth(2 * lineRadius);
+        canvas.drawPoint(centerX + realRadius, centerY, paint);
+
+        paint.setColor(Color.WHITE);
+        paint.setStrokeWidth(lineRadius);
+        canvas.drawPoint(centerX + realRadius, centerY, paint);
+
+
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        if (w != 0 && h != 0) {
+            int min = Math.min(w, h);
+            float radius = min / 2F;
+            lineRadius = radius / 16;
+            realRadius = radius - lineRadius;
+            centerX = w / 2F;
+            centerY = h / 2F;
+            rectF = new RectF(centerX - realRadius, centerY - realRadius, centerX + realRadius, centerY + realRadius);
+            sweepGradient = new SweepGradient(centerX, centerY, colors, points);
+        }
+    }
+
+    public void setScore(float score) {
+        this.score = score;
+        maxAngle = (int) (this.score / 100F * 360);
+    }
+
+    public void start() {
+        setSleepTime(20);
+        setTimes(100);
+        setClock(new OnFloatChangeListener() {
+            @Override
+            public void onChange(float value) {
+                angle = (int) (value * maxAngle);
+                if (onProgressChangeListener != null) onProgressChangeListener.onProgress(value);
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        });
+    }
+
+    private OnProgressChangeListener onProgressChangeListener;
+
+    public void setOnProgressChangeListener(OnProgressChangeListener onProgressChangeListener) {
+        this.onProgressChangeListener = onProgressChangeListener;
+    }
+
+    public interface OnProgressChangeListener {
+        void onProgress(float value);
+    }
+}
